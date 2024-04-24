44ffc3b8b795a3989303ad7428818e76798f2b5c
NazmulHasanMasad <Nazmul.Hasan@g.bracu.ac.bd>
2024-04-24 19:18:34 +0600

    .\350_intersection_of_two_arrays_solution1.cpp

--- 350_intersection_of_two_arrays_solution1.cpp
+++ 350_intersection_of_two_arrays_solution1.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
 {
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
     int n = nums1.size();
     int m = nums2.size();
 
@@ -22,7 +24,7 @@ vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
             i++;
             j++;
         }
-        else if (nums1[i < nums2[j]])
+        else if (nums1[i] < nums2[j])
         {
             i++;
         }
@@ -31,10 +33,12 @@ vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
             j++;
         }
     }
+
     for (auto x : result)
     {
         cout << x << " ";
     }
+
     return result;
 }
 
@@ -42,16 +46,17 @@ int main()
 {
     vector<int> num1;
 
-    num1.push_back(1);
-    num1.push_back(2);
-    num1.push_back(2);
-    num1.push_back(1);
+    num1.push_back(4);
+    num1.push_back(9);
+    num1.push_back(5);
 
     vector<int> num2;
 
-    num2.push_back(2);
-    num2.push_back(2);
-
+    num2.push_back(9);
+    num2.push_back(4);
+    num2.push_back(9);
+    num2.push_back(8);
+    num2.push_back(4);
     intersect(num1, num2);
 
     return 0;
