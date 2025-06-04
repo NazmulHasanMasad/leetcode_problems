d4541e0a2cd42b0d969830a9a789d7751a8436b1
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2025-06-04 15:00:58 +0600

    .\238_product_of_array_except_itshelf.cpp

---
+++ 238_product_of_array_except_itshelf.cpp
@@ -0,0 +1,14 @@
+
+int n = nums.size();
+vector<int> output(n);
+output[0] = 1;
+for (int i = 1; i < n; i++)
+{
+    output[i] = output[i - 1] * nums[i - 1];
+}
+int right = 1;
+for (int i = n - 1; i >= 0; i--)
+{
+    output[i] *= right;
+    right *= nums[i];
+}
